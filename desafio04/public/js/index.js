b46a87992de59caa3a83733998d8525f04716402
yuly-katherine <yuli-k@hotmail.com>
2023-03-09 21:02:25 -0500

    Modified public/index.js component

--- desafio04/public/js/index.js
+++ desafio04/public/js/index.js
@@ -1,11 +1,45 @@
-const socket = io();
+const socket = io()
 
-socket.on("all-productos", (data) => {
-    console.log(data);
+const containerProducts = document.getElementById("containerProducts")
 
-//   for (const el of data) {
-//     const span = document.createElement("span");
-//     span.innerText = `${el.socketId}: ${el.message}`;
-//     textInpput.appendChild(span);
-//   }
-});
+socket.on("newProduct", (data)=>{
+    containerProducts.innerHTML += `
+                                    <li>
+                                        <p><b>${data.title}</b></p>
+                                        <p>Precio: $ ${data.price}</p>
+                                        <p>Descripción: ${data.description}</p>
+                                        <p>Stock: ${data.stock}</p>
+                                        <p>Categoría: ${data.category}</p>
+                                    </li>
+                                    `
+})
+
+socket.on("deleteProduct", (products)=>{
+    containerProducts.innerHTML = ""
+    products.forEach( prod => {
+        containerProducts.innerHTML += `
+                                        <li>
+                                            <p><b>${prod.title}</b></p>
+                                            <p>Precio: $ ${prod.price}</p>
+                                            <p>Descripción: ${prod.description}</p>
+                                            <p>Stock: ${prod.stock}</p>
+                                            <p>Categoría: ${prod.category}</p>
+                                        </li>
+                                        `
+    }) 
+})
+
+socket.on("updateProduct", (products)=>{
+    containerProducts.innerHTML = ""
+    products.forEach( prod => {
+        containerProducts.innerHTML += `
+                                        <li>
+                                            <p><b>${prod.title}</b></p>
+                                            <p>Precio: $ ${prod.price}</p>
+                                            <p>Descripción: ${prod.description}</p>
+                                            <p>Stock: ${prod.stock}</p>
+                                            <p>Categoría: ${prod.category}</p>
+                                        </li>
+                                        `
+    }) 
+})
